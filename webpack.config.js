3ba2f439f22cc5c7ee8757784da54f8251c8ba1f
stubbo <georgestubbs24@gmail.com>
2021-03-01 18:51:44 +0000

    Fixed serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = { 
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true}
-                    }
-                ]
-            }
+module.exports = {
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {minimize: true}
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            tempalte: "./src/index.html",
-            filename: "./index.html"
-        })
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    })
+  ]
 }
